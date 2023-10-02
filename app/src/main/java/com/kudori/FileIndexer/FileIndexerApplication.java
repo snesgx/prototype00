a259629ebaf271e0d3119d82700a54b6b4e70ae1
Migi Ankkes <migi@ankkes>
2023-10-01 23:18:35 -0400

    Initiating SQL scripts

---
+++ app/src/main/java/com/kudori/FileIndexer/FileIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.kudori.FileIndexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileIndexerApplication.class, args);
+	}
+
+}
---
+++ sql/scratchpad.sql
@@ -0,0 +1,11 @@
+drop table your_table_name;
+
+CREATE TABLE your_table_name (
+    id SERIAL PRIMARY KEY,
+    filename VARCHAR(255),
+    filecreation_timestamp TIMESTAMP,
+    parent_id INTEGER
+);
+
+
+select * from your_table_name;
